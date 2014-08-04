20d5bd3589e669f74f976f35bd7cb379c83a55ad
derevdiggs <git@thoughtrender.com>
2014-08-04 17:10:43 -0400

    deleting files

--- upload/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-var knob = require('./jquery.knob.js');
-var widget = require('./jquery.ui.widget.js');
-var script = require('./script.js');
-var iframe = require('./jquery.iframe-transport.js');
-var fileupload = require('./jquery.fileupload.js');
